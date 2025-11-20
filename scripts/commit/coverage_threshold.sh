a73c228d0280594036a1a5b87098eb8a3b948c53
toan-local <toan@anygen.ai>
2025-11-20 16:27:46 +0700

    add 1 task & env var

---
+++ scripts/commit/coverage_threshold.sh
@@ -0,0 +1,9 @@
+COVERAGE=$(go tool cover -func=coverage.out | grep total | awk '{print $3}' | sed 's/%//')
+echo "Quality Gate: checking test coverage is above threshold ..."
+echo "Threshold                 : $COVERAGE_THRESHOLD %"
+if (( $(echo "$COVERAGE < $COVERAGE_THRESHOLD" | bc -l) ))
+then
+	echo "Coverage $COVERAGE% is below threshold $COVERAGE_THRESHOLD%"
+	exit 1
+else
+	echo "Coverage $COVERAGE% meets threshold" fi
