4425859738ea985377a5bded009708d82082f60c
toan-local <toan@anygen.ai>
2025-11-26 14:57:27 +0700

    edit

--- scripts/commit/deploy.sh
+++ scripts/commit/deploy.sh
@@ -2,28 +2,27 @@
 set -e
 
 # --- Variables from GoCD environment ---
-HELM_CHART_DIR="./helm-chart"   # path đến chart
-PIPELINE_COUNTER=${GO_PIPELINE_COUNTER}
+HELM_CHART_DIR="./helm-chart/${APP_NAME}"   # path đến chart
 REVISION=${GO_REVISION_GIT_46F3484}
 
 # --- Compute image tag ---
-IMAGE_TAG="${PIPELINE_COUNTER}-${REVISION}"
+IMAGE_TAG="${REVISION}"
 echo "Using image tag: ${IMAGE_TAG}"
 
 # --- Release and namespace ---
-RELEASE_NAME="preview-deploy-${PIPELINE_COUNTER}-${REVISION}"
-NAMESPACE="preview-deploy-${PIPELINE_COUNTER}-${REVISION}"
+RELEASE_NAME="preview-deploy-${REVISION}"
+NAMESPACE="preview-deploy-${REVISION}"
 
 echo "Release: ${RELEASE_NAME}"
 echo "Namespace: ${NAMESPACE}"
 
 # --- Values files ---
-VALUES_ARGS="-f ${HELM_CHART_DIR}/values/values.yaml -f ${HELM_CHART_DIR}/ingress-values.yaml"
+# VALUES_ARGS="-f ${HELM_CHART_DIR}/values/values.yaml -f ${HELM_CHART_DIR}/ingress-values.yaml"
 
 # --- Helm upgrade/install ---
 helm upgrade --install "${RELEASE_NAME}" "${HELM_CHART_DIR}" \
   -n "${NAMESPACE}" --create-namespace \
-  ${VALUES_ARGS} \
+#  ${VALUES_ARGS} \
   --set image.tag="${IMAGE_TAG}"
 
 echo "Deployment completed with tag: ${IMAGE_TAG}"
