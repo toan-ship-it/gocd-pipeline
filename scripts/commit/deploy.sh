c53fbe314dfb2217b6ade25aee18cb35a5f168d2
toan-local <toan@anygen.ai>
2025-12-01 10:00:08 +0700

    edit db endpoint

--- scripts/commit/deploy.sh
+++ scripts/commit/deploy.sh
@@ -5,6 +5,8 @@ set -e
 HELM_CHART_DIR="../pipeline/helm-chart/${APP_NAME}" # path đến chart
 BUILD_COUNT=${GO_DEPENDENCY_LABEL_BUILDTESTREPORT}
 REVISION=${GO_REVISION_GIT_46F3484}
+DB_HOST="172.19.80.1"
+DB_PORT="27017"
 
 # --- Compute image tag ---
 IMAGE_TAG="${BUILD_COUNT}-${REVISION}"
@@ -23,7 +25,7 @@ echo "Namespace: ${NAMESPACE}"
 # --- Helm upgrade/install ---
 helm upgrade --install "${RELEASE_NAME}" "${HELM_CHART_DIR}" \
   -n "${NAMESPACE}" --create-namespace \
-  --set mongo.uri="mongodb://127.0.0.1:27017" \
+  --set mongo.uri="mongodb://${DB_HOST}:${DB_PORT}" \
   --set image.tag="${IMAGE_TAG}" \
   --set image.repository="${DOCKERHUB_USERNAME}/${APP_NAME}"
 
